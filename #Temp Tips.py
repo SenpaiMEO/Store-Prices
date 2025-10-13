00b28a76d8f983d0a815e73e6c81c34089d2bd87
Mahmoud Essam <123819874+SenpaiMEO@users.noreply.github.com>
2025-10-13 19:42:24 +0300

    Add files via upload

---
+++ #Temp Tips.py
@@ -0,0 +1,9 @@
+Temp = 20
+if Temp >= 30:
+    print("It's a hot day, stay hydrated")
+elif Temp >= 20 and Temp < 30:
+    print("It's a warm day, enjoy the weather")
+elif Temp >= 10 and Temp <= 19:
+    print("It's a cool day, Wear a jacket")
+else:
+    print("It's a cold day, Stay warm")
